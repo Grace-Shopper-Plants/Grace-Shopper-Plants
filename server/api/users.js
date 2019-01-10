d43c72898639d40502440fa7a9b2e9877732c53b
Milly <yehm@umich.edu>
2019-01-09 19:01:46 -0500

    Added: Order Routes and Models and User Routes

--- server/api/users.js
+++ server/api/users.js
@@ -4,14 +4,34 @@ module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    })
+    const users = await User.findAll()
     res.json(users)
   } catch (err) {
     next(err)
   }
 })
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    if ((req.login && req.id) || req.isAdmin) {
+      const user = await User.findById(req.params.id)
+      res.json(user)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+// router.get('/', async (req, res, next) => {
+//   try {
+//     const users = await User.findAll({
+//       // explicitly select only the id and email fields - even though
+//       // users' passwords are encrypted, it won't help if we just
+//       // send everything to anyone who asks!
+//       attributes: ['id', 'email']
+//     })
+//     res.json(users)
+//   } catch (err) {
+//     next(err)
+//   }
+// })
