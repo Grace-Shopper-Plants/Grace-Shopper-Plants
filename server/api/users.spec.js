8e5f6cb8b5be7000cd66912c0d4237a4ca13924a
Jason Schachter <jnschachter@gmail.com>
2019-01-15 15:16:32 -0500

    Updated: user test specs back end

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -1,32 +1,43 @@
 /* global describe beforeEach it */
 
-// const {expect} = require('chai')
-// const request = require('supertest')
-// const db = require('../db')
-// const app = require('../index')
-// const User = db.model('user')
+const {expect} = require('chai')
+const request = require('supertest')
+const db = require('../db')
+const app = require('../index')
+const User = db.model('user')
 
-// describe('User routes', () => {
-//   beforeEach(() => {
-//     return db.sync({force: true})
-//   })
+describe('User routes', () => {
+  beforeEach(() => {})
 
-//   describe('/api/users/', () => {
-//     const codysEmail = 'cody@puppybook.com'
+  describe('/api/users/', () => {
+    const codysEmail = 'cody@puppybook.com'
+    const codysPassword = 'test123'
+    const codysName = 'Cody Smith'
 
-//     beforeEach(() => {
-//       return User.create({
-//         email: codysEmail
-//       })
-//     })
+    beforeEach(() => {
+      return User.create({
+        email: codysEmail,
+        password: codysPassword,
+        name: codysName
+      })
+    })
 
-//     it('GET /api/users', async () => {
-//       const res = await request(app)
-//         .get('/api/users')
-//         .expect(200)
+    it('GET /api/users', async () => {
+      const res = await request(app)
+        .get('/api/users')
+        .expect(200)
 
-//       expect(res.body).to.be.an('array')
-//       expect(res.body[0].email).to.be.equal(codysEmail)
-//     })
-//   }) // end describe('/api/users')
-// }) // end describe('User routes')
+      expect(res.body).to.be.an('array')
+      expect(res.body[0].email).to.be.equal(codysEmail)
+    })
+
+    it('GET /api/profile', async () => {
+      const res = await request(app)
+        .get('/api/users')
+        .expect(200)
+
+      expect(res.body).to.be.an('array')
+      expect(res.body[0].name).to.be.equal(codysName)
+    })
+  })
+})
